f5e3ccff8a8cf8d3caa8c80a1552e4d28df92b89
eyi739 <eyi739@gmail.com>
2025-03-28 01:09:22 -0700

    created product index component.

---
+++ views/products/index.jsx
@@ -0,0 +1,8 @@
+export default function productIndex(){
+    return (
+        <div className="App">
+            <h1>All Products</h1>
+            <p>this is where the products will be shown</p>
+        </div>
+    )
+}
