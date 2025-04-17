d164c00cd1fd947d54c27f32390f6210965ed8a3
eyi739 <eyi739@gmail.com>
2025-04-16 17:04:08 -0700

    created profiles and product route pages.

---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,6 @@
+export default function ProductsPage(){
+    const productIndex = [1,2,3,4,5]
+    return (
+        
+    )
+}
--- src/pages/ProfilesPage.jsx
+++ src/pages/ProfilesPage.jsx
@@ -1,9 +1,10 @@
+import "./ProfilesPage.css"
 import { Link } from "react-router-dom"
 
 export default function ProfilesPage(){
     const profiles = [1,2,3,4,5]
     return (
-        <div className="flex flex-col gap-6">
+        <div className="ProfilesPage">
             <h1>THIS IS THE PROFILES PAGE</h1>
             {
                 profiles.map((profile) => (
