5a9a3e3b81052f602f81da2e01e2a489698bc00a
eyi739 <eyi739@gmail.com>
2025-06-19 20:15:38 -0700

    refactoring.

--- src/client/pages/Products/ShowPage.jsx
+++ src/client/pages/Products/ShowPage.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router";
 import { Link } from "react-router-dom"
-import UsernameForm from "../Profiles/UsernameForm";
+// import UsernameForm from "../Profiles/UsernameForm";
 
 export default function ShowPage(){
     const params = useParams();
@@ -9,8 +9,10 @@ export default function ShowPage(){
         <div>
             <h1>This is the Show Page ! For this parameter: /products/{params.productId}</h1>
             <h2>{params.productId}</h2>
-            
+           <p>
             <Link to={`/products/${params.productId}/edit`}>Edit</Link>
+           </p>
+            <footer></footer>
             {/* <Outlet/> */}
         </div>
     )
