e73f20983d37813f9692b95c5776c994b3355305
eyi739 <eyi739@gmail.com>
2025-07-20 23:09:07 -0700

    styling the product index page.

--- src/client/pages/Products/ProductIndex.jsx
+++ src/client/pages/Products/ProductIndex.jsx
@@ -6,6 +6,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 
 import { Link, useNavigate } from "react-router-dom";
@@ -17,26 +18,37 @@ export default function ProductIndex(){
         <div>
             <CssBaseline/>
             <Navbar/>
-            <Box container display="flex-col" justifyContent="center" alignItems="center" >
-                <Typography variant="h1">All Products will be displayed here!!</Typography>
+            <Typography variant="h1">All Products will be displayed here!!</Typography>
+            <Grid container display="flex-col" justifyContent="center" alignItems="center" sx={{ m: 2 }} >
                 {data.map((product, index) => (
-                    
-                    <li key={index} data-id={product.id}>
-                        <Link key={product} to={`/products/${product.id}`}>
-                            {product.title}
-                        </Link>
-                    </li>
+                <Grid item key={product.id} xs={12} sm={6} md={4}>
+                    <Card>
+                        <CardMedia
+                            component="img"
+                            height="160"
+                            image={product.image}
+                            alt={product.title}
+                        />
+                        <CardContent>
+                            <Typography variant="h6">{product.title}</Typography>
+                            <Typography color="text.secondary">${product.price}</Typography>
+                            <Typography variant="body2" color="text.secondary">
+                            Category: {product.category}
+                            </Typography>
+                            <Link key={product} to={`/products/${product.id}`}>
+                                        {product.title}
+                            </Link>
+                        </CardContent>
+                    </Card>
+                </Grid>
+
+                    // <li key={index} data-id={product.id}>
+                        // <Link key={product} to={`/products/${product.id}`}>
+                        //     {product.title}
+                        // </Link>
+                    // </li>
                 ))}
-            </Box>
-            
-            
-            {/* <Outlet/> */}
-            {/* {data.map((item,index)=> (
-                <li key={index}><a href="/products/:profileId">{item.title}</a></li>
-                // <Link key={product} to={"/products/:profileId"}>
-                //         Profile {product}
-                //     </Link>
-            ))} */}
+            </Grid>
 
         </div>
     )
